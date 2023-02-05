2c936b3a54612b0d77f5003aa0221dbb9a4eda38
mendi666 <113132777+mendi666@users.noreply.github.com>
2023-02-05 16:38:22 +0200

    push

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const bodyRef = document.getElementsByTagName('body')
+
+const startBtnRef = document.getElementById('start-btn')
+const stopBtnRef = document.getElementById('stop-btn')
+
+startBtnRef.addEventListener('click', startColor)
+stopBtnRef.addEventListener('click', stopColor)
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  const colorChangeDelay = 50
+
+  let intervalID
+
+  function startColor(e) {
+ intervalID = setInterval(()=>{ 
+  document.body.style.backgroundColor = getRandomHexColor()
+  startBtnRef.setAttribute('disabled', true)
+}, colorChangeDelay)
+  }
+
+
+  function stopColor(e) {
+clearInterval(intervalID)
+startBtnRef.removeAttribute('disabled', true)
+  }
+
+
+  
+
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,123 @@
+//imports
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+//Selectors
+
+const timerRef = document.querySelector('.timer')
+const startBtnRef = document.getElementById('start-button')
+const inputRef = document.getElementById('datetime-picker')
+const daysRef = document.querySelector('[data-days]')
+const hoursRef = document.querySelector('[data-hours]')
+const minutesRef = document.querySelector('[data-minutes]')
+const secondsRef = document.querySelector('[data-seconds]')
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= new Date()) {window.alert('pick another date')
+        startBtnRef.setAttribute('disabled', true)} else {
+            startBtnRef.addEventListener('click', setTimer)
+            startBtnRef.removeAttribute('disabled', true)
+           
+            function setTimer() {
+
+                timer = setInterval(()=>{
+
+                   
+          const diff = selectedDates[0] - new Date()
+          
+           const { days, hours, minutes, seconds } = convertMs(diff)
+
+           daysRef.innerHTML = days < 10 ? addLeadingZero(days) : days;
+          hoursRef.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
+          minutesRef.innerHTML = minutes < 10 ? addLeadingZero(minutes) : minutes;
+          secondsRef.innerHTML = seconds < 10 ? addLeadingZero(seconds) : seconds;
+
+          if (diff < 1000) {
+            
+            clearInterval(timer);
+           
+          }
+
+
+                }, 1000)
+        
+
+            }
+            function addLeadingZero(value) {
+                const stringValue = String(value);
+                return stringValue.padStart(2, '0');
+              }
+              function convertMs(ms) {
+                // Number of milliseconds per unit of time
+                const second = 1000;
+                const minute = second * 60;
+                const hour = minute * 60;
+                const day = hour * 24;
+              
+                // Remaining days
+                const days = Math.floor(ms / day);
+                // Remaining hours
+                const hours = Math.floor((ms % day) / hour);
+                // Remaining minutes
+                const minutes = Math.floor(((ms % day) % hour) / minute);
+                // Remaining seconds
+                const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+              
+                return { days, hours, minutes, seconds };
+              }
+            
+
+        }  
+   
+    },
+  };
+
+  flatpickr(inputRef, options)
+
+
+
+
+
+   // addLeadingZero
+
+//    function addLeadingZero(value) {
+//     const stringValue = String(value);
+//     return stringValue.padStart(2, '0');
+//   }
+
+
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
